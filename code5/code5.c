99280e5ab5bdba395f096973a35b731967275ee5
ArtemFed <57373162+ArtemFed@users.noreply.github.com>
2023-03-10 00:09:42 +0300

    new program for 4 points

--- code5/code5.c
+++ code5/code5.c
@@ -5,27 +5,8 @@
 #include <string.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include "../func.c"
 
-const int mes_size = 5000;
-
-void count_chars(char *str) {
-    int freq[128] = {0}; // инициализация массива нулями
-
-    for (int i = 0; str[i] != '\0'; i++) {
-        freq[(int) str[i]]++; // инкремент частоты символа
-    }
-
-    char output[mes_size]; // создаем пустую строку для вывода
-    for (int i = 0; i < 128; i++) {
-        if (freq[i] > 0) {
-            char symbol[10];
-            sprintf(symbol, "%c:%d\n", i, freq[i]); // записываем символ и его частоту в строку
-            strcat(output, symbol); // добавляем символ и его частоту к строке вывода
-        }
-    }
-
-    strcpy(str, output); // копируем строку вывода обратно во входную строку
-}
 
 const char *name1 = "pipe1.fifo";
 const char *name2 = "pipe2.fifo";
